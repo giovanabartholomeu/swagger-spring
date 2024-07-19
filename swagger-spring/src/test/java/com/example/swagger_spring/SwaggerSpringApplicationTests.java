43d4e8321a7b2b5f47ddfe61497d1a6afc003f60
giovanabarth <148559217+giovanabarth@users.noreply.github.com>
2024-07-19 10:27:07 -0300

    add

---
+++ swagger-spring/src/test/java/com/example/swagger_spring/SwaggerSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.swagger_spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SwaggerSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
